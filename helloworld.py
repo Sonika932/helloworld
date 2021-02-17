737125d2ae222a6a0b83a5a62d780c664bfceafe
sonika932 <sonikasingh932@gmail.com>
2021-02-17 13:47:13 +0530

    ticket price

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,8 @@
 print('hello world')
 name = input ('what is your name?')
 print ('hello ',name)
+age= int(input('what is your age?'))
+if age <= 5: 
+    print('ticket= $5')
+else :
+    print ('ticket= $10')
