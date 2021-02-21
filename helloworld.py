916fb6c7ff8cbd51f6ab1780c59b3bb44917c66f
Sahil-484 <79013108+Sahil-484@users.noreply.github.com>
2021-02-21 21:20:05 +0530

    change ticket price to $25

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,6 @@ name = input ('what is your name?')
 print ('hello ',name)
 age= int(input('what is your age?'))
 if age <= 5: 
-    print('ticket= $5')
+    print('ticket= $15')
 else :
-    print ('ticket= $10')
+    print ('ticket= $25')
